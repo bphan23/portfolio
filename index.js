019eaa94f3560a489728be2353cab4af908b3317
bphan23 <bphan23@uw.edu>
2024-03-04 22:02:31 -0800

    Updates

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-(function() {
-
-  window.addEventListener('load', init);
+(function () {
+  window.addEventListener("load", init);
 
   /**
    * Initializes the interativity, so it's ready to call the defined anonymous
    * functions inside
    */
   function init() {
-    qs('body').classList.add('light-theme');
-    id('experience').classList.add('light-gray');
-    id('contact').classList.add('light-gray');
-    id('thanks-content-section').classList.add('light-gray');
+    qs("body").classList.add("light-theme");
+    id("experience").classList.add("light-gray");
+    id("contact").classList.add("light-gray");
+    id("thanks-content-section").classList.add("light-gray");
     // when change button is clicked - toggles between row style and column style
-    id('theme-image').addEventListener('click', changeTheme);
+    id("theme-image").addEventListener("click", changeTheme);
   }
 
   // ----------------------------------------------------------
 
   function changeTheme() {
-    let body = qs('body');
-    let themeImage = id('theme-image');
+    let body = qs("body");
+    let themeImage = id("theme-image");
     // if light theme switch to dark theme
-    if(body.classList.contains('light-theme')) {
-      body.classList.replace('light-theme', 'dark-theme');
-      id('experience').classList.replace('light-gray', 'dark-gray');
-      id('contact').classList.replace('light-gray', 'dark-gray');
-      id('thanks-content-section').classList.replace('light-gray', 'dark-gray');
+    if (body.classList.contains("light-theme")) {
+      body.classList.replace("light-theme", "dark-theme");
+      id("experience").classList.replace("light-gray", "dark-gray");
+      id("contact").classList.replace("light-gray", "dark-gray");
+      id("thanks-content-section").classList.replace("light-gray", "dark-gray");
 
       // change image *** works on local but not when I push to host
-      themeImage.src = './portfolio/images/sun.png';
-      themeImage.alt = 'sun';
+      themeImage.src = "./portfolio/images/sun.png";
+      themeImage.alt = "sun";
     } else {
       // otherwise dark
-      body.classList.replace('dark-theme', 'light-theme');
-      id('experience').classList.replace('dark-gray', 'light-gray');
-      id('contact').classList.replace('dark-gray', 'light-gray');
-      id('thanks-content-section').classList.replace('dark-gray', 'light-gray');
+      body.classList.replace("dark-theme", "light-theme");
+      id("experience").classList.replace("dark-gray", "light-gray");
+      id("contact").classList.replace("dark-gray", "light-gray");
+      id("thanks-content-section").classList.replace("dark-gray", "light-gray");
       // change image *** works on local but not when I push to host
-      themeImage.src = './portfolio/images/moon.png';
-      themeImage.alt = 'moon';
+      themeImage.src = "./portfolio/images/moon.png";
+      themeImage.alt = "moon";
     }
-
-    console.log(themeImage.src);
   }
 
   // ----------------------------------------------------------
