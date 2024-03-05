a3d0918573147a92f7c61aff393bb056eb6ec007
bphan23 <bphan23@uw.edu>
2024-03-04 21:38:37 -0800

    updated portfolio content

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,14 +1,12 @@
-
 // Home Section Component
 function Home() {
   return (
     <section id="home">
       <div id="intro-div">
-        <h1>Hi! <span class="wave">👋</span> I'm Bryan Phan</h1>
+        <h1>
+          Hi! <span class="wave">👋</span> I'm Bryan Phan
+        </h1>
         <p id="animated-text"></p>
-      </div>
-      <div id="theme-btn">
-        <img id="theme-image" src={process.env.PUBLIC_URL + '/images/moon.png'} alt="moon"/>
       </div>
     </section>
   );
