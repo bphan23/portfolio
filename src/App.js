aad1c4c9123b5f096c69b1aaa52ea50fd26da432
bphan23 <bphan23@uw.edu>
2024-03-04 21:44:28 -0800

    updated portfolio content

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import './App.css';
-import NavBar from './components/navigation/NavBar';
-import About from './components/sections/About';
-import Experience from './components/sections/Experience';
-import Projects from './components/sections/Projects';
-import Contact from './components/sections/Contact';
-import Resume from './components/sections/Resume';
-import Thanks from './components/sections/Thanks';
-import Footer from './components/utils/Footer';
-import { useEffect } from 'react';
+import "./App.css";
+import NavBar from "./components/navigation/NavBar";
+import About from "./components/sections/About";
+import Experience from "./components/sections/Experience";
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Resume from "./components/sections/Resume";
+import Thanks from "./components/sections/Thanks";
+import Footer from "./components/utils/Footer";
+import { useEffect } from "react";
 
 function App() {
   // Need to useEffect hook
   useEffect(() => {
-    let pageSections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('span.menu li a');
+    let pageSections = document.querySelectorAll("section");
+    let navLinks = document.querySelectorAll("span.menu li a");
 
     // activate link when scrolling
     window.onscroll = () => {
-
-      if(pageSections !== null)
-      pageSections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 150;
-        let height = section.offsetHeight;
-        let sectionId = section.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-
-          if(navLinks !== null)
-          navLinks.forEach(link => {
-            link.classList.remove('active'); // remove
-            document.querySelector('span.menu li a[href*=' + sectionId + ']').classList.add('active'); // add
-          });
-        }
-      });
-    }
-  })
+      if (pageSections !== null)
+        pageSections.forEach((section) => {
+          let top = window.scrollY;
+          let offset = section.offsetTop - 150;
+          let height = section.offsetHeight;
+          let sectionId = section.getAttribute("id");
+          if (top >= offset && top < offset + height) {
+            if (navLinks !== null)
+              navLinks.forEach((link) => {
+                link.classList.remove("active"); // remove
+                document
+                  .querySelector("span.menu li a[href*=" + sectionId + "]")
+                  .classList.add("active"); // add
+              });
+          }
+        });
+    };
+  });
 
   return (
     <div>
-      <NavBar/>
-      <About/>
-      <Experience/>
-      <Projects/>
-      <Contact/>
-      <Resume/>
-      <Thanks/>
-      <Footer/>
+      <NavBar />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Resume />
+      <Thanks />
+      <Footer />
     </div>
   );
 }
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,25 +1,43 @@
-import Home from '../sections/Home';
-import './NavBar.css';
+import Home from "../sections/Home";
+import "./NavBar.css";
 
 function Navbar() {
   return (
     <header>
       <nav>
         <ul class="nav-bar">
-          <li class="logo"><a href="#">Bryan Phan</a></li>
+          <li class="logo">
+            <a href="#">Bryan Phan</a>
+          </li>
           <input type="checkbox" id="check" />
           <span class="menu">
-            <li><a class="active" href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+            <li>
+              <a class="active" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <label for="check" class="close-menu">
+              <i class="fas fa-times"></i>
+            </label>
           </span>
-          <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+          <label for="check" class="open-menu">
+            <i class="fas fa-bars"></i>
+          </label>
         </ul>
       </nav>
-      <Home/>
+      <Home />
     </header>
   );
 }
